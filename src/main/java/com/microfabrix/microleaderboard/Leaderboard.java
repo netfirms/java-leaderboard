79fdc84399b4f0463a388da256d9f54f978387fb
Taweechai Maklay <m.taweechai@gmail.com>
2019-01-02 14:59:35 +0700

    - Add new version number. - Added .gitignore.

--- src/main/java/com/microfabrix/microleaderboard/Leaderboard.java
+++ src/main/java/com/microfabrix/microleaderboard/Leaderboard.java
@@ -8,7 +8,7 @@ import redis.clients.jedis.Transaction;
 
 public class Leaderboard {
 
-	public static final String VERSION = "2.0.2";
+	public static final String VERSION = "2.0.3";
 	public static final int DEFAULT_PAGE_SIZE = 25;
 	public static final String DEFAULT_REDIS_HOST = "localhost";
 	public static final int DEFAULT_REDIS_PORT = 6379;
